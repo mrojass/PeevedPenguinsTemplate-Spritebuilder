d1d72238bf0814b36d36576266de81e425d37d64
Michelle <mrojass1028@gmail.com>
2014-07-30 19:38:24 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Michelle Rojas on 7/30/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
