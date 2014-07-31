0f43ae6ffd0b6048a2852e5836da77d5067a2e0b
Michelle <mrojass1028@gmail.com>
2014-07-30 23:58:35 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -14,6 +14,17 @@
     CCNode        *_levelNode;
 }
 
+-(Gameplay*)init {
+    self = [super init];
+    if (self) {
+        CCButton *retryButton = [CCButton buttonWithTitle:@"Retry"];
+        retryButton.anchorPoint = ccp(0,0);
+        retryButton.position= ccp(self.contentSize.width - 130, 120);
+    }
+    
+    return self;
+}
+
 // is called when CCB file has completed loading
 -(void)didLoadFromCCB {
     // tell this scene to accept touches
@@ -21,6 +32,7 @@
     
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+
 }
 
 // called on every touch in this scene
